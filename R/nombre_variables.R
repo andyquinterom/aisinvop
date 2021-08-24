b0df7e44e99801a998ea12149702c9f1f7bf713d
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-08-24 13:06:12 -0500

    refactor: Adecua a estructura del modelo del problema
    Adelanto B5

--- R/nombre_variables.R
+++ R/nombre_variables.R
@@ -1,20 +1,22 @@
 
- # Función nombre_variables sirve para almacenar los nombres de  variables del
+
+# Función nombre_variables sirve para almacenar los nombres de  variables del
 # modelo a resolver. Estos nombres se usaran en las operaciones matriciales en
 # las restricciones y función objetivo
 
 nombres_variables <- function(
-  # modelo = "",
-  variables,
-  indices = list(),
-  type = ""
-  ){
-    var <- enquo(variables)
-    nombre_variables <- indices %>% 
-      cross %>% map(lift(paste)) %>% 
-      unlist() %>% 
-      stringr::str_replace(" ",",") %>% 
-      paste0(as_label(var),"[",.,"]")
-    
+  modelo
+){
+  var <- modelo$variables
+  nombre_variables <- 
+    var %>% purrr::map(function(x){
+      modelo$indices_variables %>% 
+        purrr::map(function(y) cross(y) %>% map(lift(paste)))  %>% 
+        pluck(x) %>% unlist() %>% 
+        stringr::str_replace(" ",",") %>%
+        paste0(x,"[",.,"]")
+})
+
+return(nombre_variables)    
 }
 
